cd26475960fedc01208cb06f59dc53e972f5fe3e
shubhamHaral <106648062+shubhamHaral@users.noreply.github.com>
2026-08-14 01:27:32 +0530

    Fix Render frontend host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
   preview: {
     host: "0.0.0.0",
     port: 4173,
-    allowedHosts: [
-      "salesforce-crud-frontend-92hv.onrender.com"
-    ]
-  }
+    allowedHosts: true,
+  },
 });
